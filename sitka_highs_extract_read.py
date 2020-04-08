8d27cee53bfad8d075058aabab49e4158af879c4
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-08 14:51:36 -0400

    Extract and read specific values in a CSV file using a for loop and row() function

---
+++ sitka_highs_extract_read.py
@@ -0,0 +1,28 @@
+# Comma-Separated Values (CSV) Parsing: Extract and Read Data
+
+# Imports the Python CSV module
+import csv
+
+# Specifies the file we want to open
+filename = 'data/sitka_weather_07-2018_simple.csv'
+
+# Opens the file with the path in the filename variable
+with open(filename) as f:
+
+    """ Uses reader to read and parse the CSV file """
+    reader = csv.reader(f)
+
+    # next() function in CSV module reads each line
+    # Here we read the first line to get the header row and print it out in the terminal
+    header_row = next(reader)
+    
+    # Get high temperatures from this file
+    highs = []
+
+    # Loops through every line in reader dictionary (where each row is a list of values separated by a comma)
+    for row in reader:
+        high = int(row[5]) # Sets the value in position five of the row to 'high' variable
+        highs.append(high) # Appends the 'high' variable value to the highs list
+
+    print(highs)
+    
