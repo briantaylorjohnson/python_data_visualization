448ad5ed884c885ea3d23dba9eebb7c2e2222164
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-08 02:37:48 -0400

    Plotly visualization if two D6 dice are rolled 1,000 times; output to HTML file for viewing in a browser

--- die_visual.py
+++ die_visual.py
@@ -29,6 +29,6 @@ data = [Bar(x=x_values, y=frequencies)]
 x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title='Results of rolling one D6 1,000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+my_layout = Layout(title='Results of rolling one D6 dice 1,000 times', xaxis=x_axis_config, yaxis=y_axis_config)
 
 offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
