c5eeeb87694fa83bf9f485643f67febdf12867cf
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-08 20:16:30 -0400

    Plot earthquake data on world map using longitude and latitude; output to HTML file

---
+++ eq_world_map.py
@@ -0,0 +1,42 @@
+# Mapping Global Data Sets: JSON Format
+
+# Import the Python JSON module
+import json
+
+# Import Plotly modules for visualizations and offline output
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the structure of the data
+filename = 'data/eq_data_1_day_m1.json'
+
+# Opens the file and extracts all data within
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+# Extracts all earthquake dictionaries in the data source
+all_eq_dicts = all_eq_data['features']
+
+# Extracts all earthquake magnitudes from each earthquake dictionary
+mags = []
+
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+# Extracts all location data from each earthquake dictionary
+lons, lats = [], []
+
+for eq_dict in all_eq_dicts:
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    lons.append(lon)
+    lats.append(lat)
+
+# Map the earthquakes
+data =[Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
+
