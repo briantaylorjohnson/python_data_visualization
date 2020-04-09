3667fc57865893104f0eaf80c04f109f1b7d391a
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-08 20:46:03 -0400

    Making an API call with Python using Requests module; store and output response

---
+++ python_repos.py
@@ -0,0 +1,16 @@
+# Working with APIs: Processing an API Response
+
+# Import Python Requests module
+import requests
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status Code: {r.status_code}")
+
+# Store API response in a variable
+response_dict = r.json()
+
+# Process results
+print(response_dict.keys())
